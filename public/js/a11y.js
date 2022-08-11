c0f7902850168cfe1baf0b1c934ee62b6c2d7387
Erissa Renée Duvall <corgidev@outlook.com>
2022-08-11 16:59:28 -0400

    Cleaned up CSS of unused styles and troubleshooting JS.

--- public/js/a11y.js
+++ public/js/a11y.js
@@ -1,15 +1,12 @@
 (function () {
-    const headings = document.querySelectorAll('h2')
-
-    Array.prototype.forEach.call(headings, h => {
-      let btn = h.querySelector('button')
-      let target = h.nextElementSibling
-
-      btn.onclick=()=> {
-        let expanded = btn.getAttribute('aria-expanded') === 'true'
-
-        btn.setAttribute('aria-expanded', !expanded)
-        target.hidden = expanded
-      }
-    })
-  })()
+  const headings = document.querySelectorAll('h2');
+  Array.prototype.forEach.call(headings, h => {
+     let btn = h.querySelector('button');
+     let target = h.nextElementSibling;
+     btn.onclick = () => {
+         let expanded = btn.getAttribute('aria-expanded') === 'true';
+         btn.setAttribute('aria-expanded', !expanded);
+         target.hidden = expanded;
+     }
+ });
+})();
